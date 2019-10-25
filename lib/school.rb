c37f44406c19c0dfe7e4ca2f319d1506ac2ee269
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-10-25 02:47:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,12 @@ class School
   end
   
   def add_student(student, grade)
-    
+    if roster.has_key?(grade)
+      roster[grade] << student
+    else
+      roster[grade] = []
+      add_student(student, grade)
+    end
   end
     
   
